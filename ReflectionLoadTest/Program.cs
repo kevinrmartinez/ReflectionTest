0a184ed48a6fab1531c35881f61c47ae15cf07fe
kevinrmartinez <kevinrmartinezj@gmail.com>
2023-10-03 22:49:44 -0400

    Añadido manejo de argumentos

--- ReflectionLoadTest/Program.cs
+++ ReflectionLoadTest/Program.cs
@@ -16,7 +16,12 @@ for (int i = 0; i < Pack.Length; i++)
     Console.WriteLine(Pack[i].ToString());
 }
 Console.WriteLine();
-
+var len = args.Length;
+string argument = string.Empty;
+for (int i = 0; i < len; i++)
+{
+    argument += args[i];
+}
 
 Type[] methodTypes1 = new Type[]
     { };
@@ -25,6 +30,9 @@ Type[] methodTypes2 = new Type[]
     { typeof(string) };
 MethodInfo Pack1 = ReflectionPackTestType.GetMethod("Pack1", methodTypes2);
 Pack1.Invoke(instancia, new object[] { "bitch" });
+if (!string.IsNullOrEmpty(argument))
+{ Pack1.Invoke(instancia, new object[] { argument }); }
+
 Type[] methodTypes3 = new Type[]
     { typeof(IList<Object>) };
 
